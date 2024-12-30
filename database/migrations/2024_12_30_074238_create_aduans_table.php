9952e421ef47df1650619c451909741b7631e608
hazimah-uitm <hazimahpethie@uitm.edu.my>
2024-12-30 17:01:37 +0800

    Added Aduan data

---
+++ database/migrations/2024_12_30_074238_create_aduans_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAduansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aduans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('aduan_ict_tiket')->nullable();
+            $table->string('complainent_name_id')->nullable();
+            $table->string('complainent_category')->nullable();
+            $table->string('aduan_category')->nullable();
+            $table->string('aduan_subcategory')->nullable();
+            $table->string('campus')->nullable();
+            $table->string('location')->nullable();
+            $table->text('aduan_details')->nullable();
+            $table->string('aduan_status')->nullable();
+            $table->string('aduan_type')->nullable();
+            $table->string('staff_duty')->nullable();
+            $table->text('remark_staff_duty')->nullable();
+            $table->date('date_applied')->nullable();
+            $table->time('time_applied')->nullable();
+            $table->date('date_completed')->nullable();
+            $table->time('time_completed')->nullable();
+            $table->string('response_time')->nullable();
+            $table->integer('rating')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aduans');
+    }
+}
