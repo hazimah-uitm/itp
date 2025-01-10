69d156beff7335589905d89b82225525b7d6121f
hazimah-uitm <hazimahpethie@uitm.edu.my>
2025-01-10 11:01:55 +0800

    updated home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Aduan;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,34 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $aduanList = Aduan::all();
+
+        // Total Aduan
+        $totalAduan = $aduanList->count();
+
+        // Aduan by Status
+        $aduanCompleted = $aduanList->whereIn('aduan_status', ['ADUAN COMPLETED', 'ADUAN VERIFIED'])->count();
+        $inProgress = $aduanList->whereIn('aduan_status', ['IT SERVICES - 2ND LEVEL SUPPORT', '2ND LEVEL MAINTENANCE', '1ST LEVEL SUPPORT'])->count();
+        $cancelled = $aduanList->where('aduan_status', 'ADUAN CANCELLED')->count();
+        $closed = $aduanList->where('aduan_status', ['ADUAN CLOSED', 'ADUAN CLOSED (INCOMPLETE INFORMATION / WRONG CHANNEL)'])->count();
+
+        // Percentage calculation
+        $percentAduanCompleted = ($totalAduan > 0) ? round(($aduanCompleted / $totalAduan) * 100, 2) : 0;
+        $percentInProgress = ($totalAduan > 0) ? round(($inProgress / $totalAduan) * 100, 2) : 0;
+        $percentCancelled = ($totalAduan > 0) ? round(($cancelled / $totalAduan) * 100, 2) : 0;
+        $percentClosed = ($totalAduan > 0) ? round(($closed / $totalAduan) * 100, 2) : 0;
+
+        return view('home', [
+            'aduanList' => $aduanList,
+            'totalAduan' => $totalAduan,
+            'aduanCompleted' => $aduanCompleted,
+            'inProgress' => $inProgress,
+            'cancelled' => $cancelled,
+            'closed' => $closed,
+            'percentAduanCompleted' => $percentAduanCompleted,
+            'percentInProgress' => $percentInProgress,
+            'percentCancelled' => $percentCancelled,
+            'percentClosed' => $percentClosed,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,60 @@
 @extends('layouts.master')
+
 @section('content')
 <div class="container-fluid mb-3">
-    <div class="row">
-        <div class="col">
-            <p>Logged in</p>
+    <div class="row mb-4">
+        <!-- Jumlah Aduan ICT (Main Card) -->
+        <div class="col-md-4 mb-3">
+            <div class="card text-white bg-primary" style="font-size: 1.5rem;">
+                <div class="card-header text-center">Jumlah Aduan ICT</div>
+                <div class="card-body text-center">
+                    <h1 class="card-title display-4">{{ $totalAduan }}</h1>
+                </div>
+            </div>
+        </div>
+
+        <!-- Aduan Completed Card -->
+        <div class="col-md-2 mb-3">
+            <div class="card text-white bg-success">
+                <div class="card-header text-center">Aduan Completed</div>
+                <div class="card-body text-center">
+                    <h5 class="card-title">{{ $aduanCompleted }}</h5>
+                    <p class="text-white">{{ $percentAduanCompleted }}%</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- In Progress Card -->
+        <div class="col-md-2 mb-3">
+            <div class="card text-white bg-warning">
+                <div class="card-header text-center">In Progress</div>
+                <div class="card-body text-center">
+                    <h5 class="card-title">{{ $inProgress }}</h5>
+                    <p class="text-white">{{ $percentInProgress }}%</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Cancelled Card -->
+        <div class="col-md-2 mb-3">
+            <div class="card text-white bg-danger">
+                <div class="card-header text-center">Cancelled</div>
+                <div class="card-body text-center">
+                    <h5 class="card-title">{{ $cancelled }}</h5>
+                    <p class="text-white">{{ $percentCancelled }}%</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Closed Card -->
+        <div class="col-md-2 mb-3">
+            <div class="card text-white bg-secondary">
+                <div class="card-header text-center">Closed</div>
+                <div class="card-body text-center">
+                    <h5 class="card-title">{{ $closed }}</h5>
+                    <p class="text-white">{{ $percentClosed }}%</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
